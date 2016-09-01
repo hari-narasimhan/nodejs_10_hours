a99bfa6f136c40d80e47d8c64c1ffdbf4874e7ac
Hari Narasimhan <001116@q4-chn-l-168.chennai.qfor.com>
2016-09-01 13:02:20 +0530

    example tweaked to send smaller chunks

--- hour_06/video-server.js
+++ hour_06/video-server.js
@@ -1,36 +1,41 @@
-"use strict";
+'use strict';
 
-var fs = require("fs"),
-    http = require("http"),
-    url = require("url"),
-    path = require("path");
+var fs = require('fs');
+var http = require('http');
+var url = require('url');
+var path = require('path');
 
 http.createServer(function (req, res) {
-  if (req.url != "/movie.mp4") {
-    res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url != '/movie.mp4') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<video src="http://localhost:8888/movie.mp4" controls></video>');
   } else {
-    var file = path.resolve(__dirname,"movie.mp4");
+    var file = path.resolve(__dirname, 'movie.mp4');
     var range = req.headers.range;
-    var positions = range.replace(/bytes=/, "").split("-");
+    var positions = range.replace(/bytes=/, '').split('-');
     var start = parseInt(positions[0], 10);
 
-    fs.stat(file, function(err, stats) {
+    fs.stat(file, function (err, stats) {
       var total = stats.size;
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var end = positions[1] ?
+        parseInt(positions[1], 10) : (start + (2048 * 10)) - 1;
+
+      // Ensure end does not exceed the size
+      end = end > total ? (total - 1) : end;
+
       var chunksize = (end - start) + 1;
 
       res.writeHead(206, {
-        "Content-Range": "bytes " + start + "-" + end + "/" + total,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
+        'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
       });
 
       var stream = fs.createReadStream(file, { start: start, end: end })
-        .on("open", function() {
+        .on('open', function () {
           stream.pipe(res);
-        }).on("error", function(err) {
+        }).on('error', function (err) {
           res.end(err);
         });
     });
